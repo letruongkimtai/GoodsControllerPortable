103086bcd5fb0b910ba54ed7d5928883f049d533
Kim Tai <letruongkimtai@gmail.com>
2019-05-29 09:35:38 +0700

    API init

--- GCPv3/src/components/order/OrderDetail.js
+++ GCPv3/src/components/order/OrderDetail.js
@@ -56,8 +56,6 @@ const styles = StyleSheet.create({
         flex: 1,
         width: '100%',
         height: '100%',
-        // fontSize:17,
-        // color:"black",
     },
     Order: {
         height: '100%',
