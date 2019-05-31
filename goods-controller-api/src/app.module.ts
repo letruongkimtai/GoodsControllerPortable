ab6a53bf2308af600bb97a0757320d5339f58f32
Kim Tai <letruongkimtai@gmail.com>
2019-05-31 14:40:06 +0700

    Revert "error"
    This reverts commit 9be1fb67fda34ade9f411b2f7f12ecf23962594f.

--- goods-controller-api/src/app.module.ts
+++ goods-controller-api/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'//import nestJS typeorm 
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { ProductTypeModule } from './product-type/product-type.module';
 import { BrandModule } from './brand/brand.module';
 import { ProductModule } from './product/product.module';
